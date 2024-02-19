13ecc6256e42abbfd09863de92bce44600407688
Ahmed Noorim <ahmed.noorim13@gmail.com>
2024-02-19 06:06:40 +0600

    ticket.js - seatSelection() updated

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,4 +1,42 @@
 const selectedSeats = []
+const validCoupons = ['NEW15', 'COUPLE20']
+
+// get coupon
+const couponApply = getElementById("coupon-apply")
+
+function couponChecker() {
+    let coupon = getElementById("coupon-input")
+    coupon = coupon.value
+    coupon = coupon.split(" ").join("").toUpperCase()
+
+    if (validCoupons.includes(coupon)) {
+        hideElement("coupon-apply-div")
+        unHideElement("discount")
+
+        let discount = coupon.substring(coupon.length - 2)
+        discount = parseInt(discount) / 100
+
+        const discountAmount = 550 * selectedSeats.length * discount
+        setTextById("discount-amount", discountAmount)
+
+        const grandTotal = 550 * selectedSeats.length - discountAmount
+        setTextById("grand-total", grandTotal)
+
+        return true
+    }
+    else
+        return false
+}
+
+couponApply.addEventListener('click', function () {
+
+    if (couponChecker()) {
+    }
+    else {
+        alert("Wrong! Give a valid coupon.")
+    }
+})
+
 
 // add seating
 function seatsAdded(seat) {
@@ -27,21 +65,28 @@ function seatSelection() {
     const seats = document.getElementsByClassName("seat")
     for (const seat of seats) {
         seat.addEventListener('click', function () {
-            console.log("Seat Selected:", seat.id)
+            const seatID = seat.id
+            console.log("Seat Selected:", seatID)
             // console.log(selectedSeats)
 
             // checking 4 seat selection
             if (selectedSeats.length < 4) {
-                selectedSeats.push(seat.id)
-                setBackGroundColorById(seat.id, '[#1DD100]')
+                if (!selectedSeats.includes(seatID)) {
+                    selectedSeats.push(seat.id)
+                    setBackGroundColorById(seat.id, '[#1DD100]')
 
-                const totalSeats = 40
-                setTextById('seat-left', totalSeats - selectedSeats.length)
+                    const totalSeats = 40
+                    setTextById('seat-left', totalSeats - selectedSeats.length)
 
-                setTextById('seat-count', selectedSeats.length)
+                    setTextById('seat-count', selectedSeats.length)
 
-                seatsAdded(seat.id)
+                    seatsAdded(seat.id)
 
+                    setTextById("total-price", 550 * selectedSeats.length)
+                    setTextById("grand-total", 550 * selectedSeats.length)
+
+                    couponChecker()
+                }
             }
             else {
                 alert("You have already selected 4 tickets!")
